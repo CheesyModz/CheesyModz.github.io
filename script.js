42bf0676f4902274fadc113137da2bff4da3357e
CheesyModz <49135331+CheesyModz@users.noreply.github.com>
2021-10-18 11:42:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-var img_array = ["images/ganyu.jpg", "images/miku.png", "images/ganyu1.jpg", "images/ayaka.jpg"];
+var imgArray = [
+                "images/ganyu.jpg", 
+                "images/miku.png", 
+                "images/ganyu1.jpg", 
+                "images/ayaka.jpg"
+                ];
 i = 0;
 
 function myFunction() {
     i++;
-    document.getElementById("myImg").src = img_array[i];
-    if (i == img_array.length - 1) {
+    document.getElementById("myImg").src = imgArray[i];
+    if (i == imgArray.length - 1) {
         i = -1;
-
     }
+}
 
+var possibleResponses = [
+                        "That is a no",
+                        "It is not looking likely",
+                        "Too hard to tell",
+                        "It is quite possible",
+                        "Definitely"
+                        ];
+
+function response() {
+    document.getElementById("test").innerHTML = possibleResponses[(Math.floor(Math.random() * 5))];
 }
