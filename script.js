30253b9abe849c9d917eff3a55758f098877d199
CheesyModz <49135331+CheesyModz@users.noreply.github.com>
2021-11-05 11:28:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var possibleResponses = [
                         ];
 
 function response() {
-    document.getElementById("test").innerHTML = possibleResponses[(Math.floor(Math.random() * 5))];
+    document.getElementById("output").innerHTML = possibleResponses[(Math.floor(Math.random() * 5))];
 }
 
 function openNav() {
@@ -32,4 +32,64 @@ function openNav() {
 
 function closeNav () {
     document.getElementById("mySidenav").style.width = "0";
+}
+
+
+function startGame() {
+    box = new component();
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component() {
+    this.width = 30;
+    this.height = 30;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = 10;
+    this.y = 120;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    box.newPos();    
+    box.update();
+}
+
+function moveup() {
+    box.speedY -= 1; 
+}
+
+function movedown() {
+    box.speedY += 1; 
+}
+
+function moveleft() {
+    box.speedX -= 1; 
+}
+
+function moveright() {
+    box.speedX += 1; 
 }
